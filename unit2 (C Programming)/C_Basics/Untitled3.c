011bdcdfc746fd3209f935902960a0f0bb7c88a2
Abdelrahman5199 <bodykotb564@gmail.com>
2022-09-05 13:59:15 +0200

    unit2/

---
+++ unit2 (C Programming)/C_Basics/Untitled3.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main()
+{
+
+char fname[20],mname[20],lname[20];
+printf("Enter the First Name Middle Name& Last Name");
+scanf("%s %s %s",fname,mname,lname);
+printf("Abbreviated name:");
+printf("%c. %c. %s\n",fname[0],mname[0],lname);
+return 0;
+}
